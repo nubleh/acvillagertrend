48262bf71aa36eb4a0afd7a0dd1e8b9a4bb15336
Nubli <nubli.naim@gmail.com>
2020-05-17 13:51:31 +0900

    chore: build into docs

---
+++ docs/precache-manifest.5513c4b5f1c4ed0fa261faa08eb61e17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f391e03c656520c426ac743d6abc6dd6",
+    "url": "/acvillagertrend/index.html"
+  },
+  {
+    "revision": "bcfae5adb8883386becc",
+    "url": "/acvillagertrend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1523ab80657674133d10",
+    "url": "/acvillagertrend/static/js/2.44607844.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/acvillagertrend/static/js/2.44607844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcfae5adb8883386becc",
+    "url": "/acvillagertrend/static/js/main.01750961.chunk.js"
+  },
+  {
+    "revision": "f1b11987a2b58237bba3",
+    "url": "/acvillagertrend/static/js/runtime-main.7302699d.js"
+  }
+]);
